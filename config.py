bf2be3b944bea3a6807f27163cb68d471710804b
ngd527 <ngd-am21@student.tarc.edu.my>
2024-09-19 14:38:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c6l0ycydm9zz.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ngdesmond-employee"
+customregion = "us-east-1"
+
